adf5b2e678a7d102952f6003ae85edc564fd5d31
Fariha Sultana <farihasultana237@gmail.com>
2023-06-28 20:56:00 +0600

    #4 Added Image and Background Color to the Login page

--- src/electricity/billing/system/Login.java
+++ src/electricity/billing/system/Login.java
@@ -9,6 +9,8 @@ public class Login extends JFrame {
     Login(){
         super("Login");
 
+        getContentPane().setBackground(Color.LIGHT_GRAY);
+
         JLabel username = new JLabel("Username");
         username.setBounds(300,60, 100, 20);
         add(username);
@@ -34,6 +36,14 @@ public class Login extends JFrame {
         loginChoice.add("Customer");
         loginChoice.setBounds(400, 140, 100, 20);
         add(loginChoice);
+
+
+        ImageIcon profileOne = new ImageIcon(ClassLoader.getSystemResource("icon/Login/profile.png"));
+        Image profile1 = profileOne.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon fprofile1 = new ImageIcon(profile1);
+        JLabel profileLabel = new JLabel(fprofile1);
+        profileLabel.setBounds(10,5, 250, 250);
+        add(profileLabel);
 
 
         setSize(640, 300);
